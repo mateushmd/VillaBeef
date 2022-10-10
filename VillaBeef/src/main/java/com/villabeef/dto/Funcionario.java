f6cd63cca64471b0cf590eb38771b42206d9ab2c
Aluno <Aluno@DECOM-LAB-049>
2022-10-10 11:31:10 -0300

    Implementação Funcionário DTO #4

--- VillaBeef/src/main/java/com/villabeef/dto/Funcionario.java
+++ VillaBeef/src/main/java/com/villabeef/dto/Funcionario.java
@@ -1,5 +1,40 @@
-package com.villabeef.dto;
+        package com.villabeef.dto;
 
 public class Funcionario extends Usuario{
     
+    private String nome;
+    private double salario;
+    private double conta;        
+    
+    public Funcionario(String nome, double salario, double conta) {
+        this.nome = nome;
+        this.salario = salario;
+        this.conta = conta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public double getConta() {
+        return conta;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void setConta(double conta) {
+        this.conta = conta;
+    }
 }
+
+
