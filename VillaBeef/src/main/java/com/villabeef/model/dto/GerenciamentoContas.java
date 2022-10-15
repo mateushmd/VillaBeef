21aef9434028178d14bb2387312efd3052289604
Samsung <Samsung@Davi>
2022-10-15 20:36:53 -0300

    #21 conta DTO

---
+++ VillaBeef/src/main/java/com/villabeef/model/dto/GerenciamentoContas.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.villabeef.model.dto;
+
+
+public class GerenciamentoContas {
+    private String data;
+    private char tipo; //e = entrada; s = saida;
+    private double valor;
+}
