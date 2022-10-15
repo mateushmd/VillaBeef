060c3031fd65789b89efa2ddad88750453cceed0
Samsung <Samsung@Davi>
2022-10-15 20:44:41 -0300

    #21 conta DTO com getter e setter

--- VillaBeef/src/main/java/com/villabeef/model/dto/GerenciamentoContas.java
+++ VillaBeef/src/main/java/com/villabeef/model/dto/GerenciamentoContas.java
@@ -9,4 +9,34 @@ public class GerenciamentoContas {
     private String data;
     private char tipo; //e = entrada; s = saida;
     private double valor;
+
+    public GerenciamentoContas(String data, char tipo, double valor) {
+        this.data = data;
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
 }
