3cee0d6507251bebbb06dab28d503ef617cd098a
Mateus <>
2022-11-01 10:37:34 -0300

    Reestruturação do projeto #60

---
+++ VillaBeef/Model/src/main/java/com/villabeef/model/service/ManterProduto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.villabeef.model.service;
+
+
+public class ManterProduto {
+
+    private ManterProduto() {
+        
+    }
+    public static void adicionarProduto(int id, int idProduto, String validade, double valor) {
+        
+    }
+    
+}
