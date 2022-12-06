1e426733273c28394bb1955512ba1fc5e8ae0df4
Davi <>
2022-12-06 19:33:50 -0300

    Correções e mudanças #107

---
+++ VillaBeef/Model/src/main/java/com/villabeef/model/dto/Imposto.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.villabeef.model.dto;
+
+
+public class Imposto 
+{
+    String descricao;
+    double valor;
+    
+    public Imposto(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
